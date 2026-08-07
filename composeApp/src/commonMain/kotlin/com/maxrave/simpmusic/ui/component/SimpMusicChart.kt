8a229c673a5b99226a045dc84afc1c1bad611a1e
themagmalord333-oss <the.magma.lord.333@gmail.com>
2026-08-08 03:27:51 +0530

    Update SimpMusicChart.kt

--- composeApp/src/commonMain/kotlin/com/maxrave/simpmusic/ui/component/SimpMusicChart.kt
+++ composeApp/src/commonMain/kotlin/com/maxrave/simpmusic/ui/component/SimpMusicChart.kt
@@ -52,7 +52,7 @@ fun SimpMusicChartButton(
 
             // Text
             Text(
-                text = "Introducing SimpMusic Chart",
+                text = "Introducing MagmaMusic Chart",
                 fontSize = 13.sp,
                 color = labelColor,
                 fontWeight = FontWeight.Normal
