a9cd6df8f4508788fef312fe52216bc04a0ab8db
themagmalord333-oss <the.magma.lord.333@gmail.com>
2026-08-08 04:51:13 +0530

    Update BlogPromoDialog.kt

--- composeApp/src/commonMain/kotlin/com/maxrave/simpmusic/ui/component/BlogPromoDialog.kt
+++ composeApp/src/commonMain/kotlin/com/maxrave/simpmusic/ui/component/BlogPromoDialog.kt
@@ -38,7 +38,7 @@ fun BlogPromoDialog(
         confirmButton = {
             TextButton(onClick = {
                 onVisitBlog.invoke()
-                uriHandler.openUri("https://maxrave.dev")
+                uriHandler.openUri("https://magma-portfolio-sigma.vercel.app")
             }) {
                 Text(
                     stringResource(Res.string.visit_blog),
