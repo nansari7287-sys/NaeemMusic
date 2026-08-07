57517fb8bbcec077494cbdcc44e17710adcba9d0
themagmalord333-oss <the.magma.lord.333@gmail.com>
2026-08-08 02:55:26 +0530

    Update EndOfPage.kt

--- composeApp/src/commonMain/kotlin/com/maxrave/simpmusic/ui/component/EndOfPage.kt
+++ composeApp/src/commonMain/kotlin/com/maxrave/simpmusic/ui/component/EndOfPage.kt
@@ -34,7 +34,7 @@ fun EndOfPage(withoutCredit: Boolean = false) {
                     stringResource(
                         Res.string.version_format,
                         VersionManager.getVersionName(),
-                    ) + "\nmaxrave-dev",
+                    ) + "\nmagma-dev",
                 style = typo().bodySmall,
                 textAlign = TextAlign.Center,
                 modifier =
