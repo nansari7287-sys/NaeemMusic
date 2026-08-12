39ef33c43e2df54d420de36d9051bfaab4b86f0d
nansari7287-sys <nansari7287@gmail.com>
2026-08-12 13:47:24 +0530

    Create composeApp/src/commonMain/kotlin/com/maxrave/simpmusic/ui/screen/other/VideoPlayer.kt

---
+++ composeApp/src/commonMain/kotlin/com/maxrave/simpmusic/ui/screen/other/VideoPlayer.kt
@@ -0,0 +1,7 @@
+package com.maxrave.simpmusic.ui.screen.other
+
+import androidx.compose.runtime.Composable
+
+// Yeh bas ek rule hai ki alag-alag device par alag background chalega
+@Composable
+expect fun DynamicBackground()
