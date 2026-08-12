9f394fe66ef6d08e221c00cb76b18bb45f1cf7bb
nansari7287-sys <nansari7287@gmail.com>
2026-08-12 14:03:23 +0530

    Delete VideoPlayer.kt

--- composeApp/src/commonMain/kotlin/com/maxrave/simpmusic/ui/screen/other/VideoPlayer.kt
+++
@@ -1,7 +0,0 @@
-package com.maxrave.simpmusic.ui.screen.other
-
-import androidx.compose.runtime.Composable
-
-// Yeh bas ek rule hai ki alag-alag device par alag background chalega
-@Composable
-expect fun DynamicBackground()
